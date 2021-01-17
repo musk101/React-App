3e729cd84b96f7d0816119affacfe0e325f3b589
agarwalminks <61506798+agarwalminks@users.noreply.github.com>
2021-01-17 18:25:23 +0530

    new app

--- components/src/index.js
+++ components/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard'
 const App=function(){
     return <div className='ui container elements'>
-   <CommentDetail/>
+        <ApprovalCard>
+   <CommentDetail/></ApprovalCard>
    <CommentDetail/>
    <CommentDetail/>
 </div>
---
+++ pics/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import app from './components/app';
+
+ReactDOM.render(
+    <app/>,
+  document.getElementById('root')
+);
+
+
