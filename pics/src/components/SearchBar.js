71aafb48b3e72c21ce1182ef794108ce4620612a
agarwalminks <61506798+agarwalminks@users.noreply.github.com>
2021-01-19 04:30:12 +0530

    axios learn in pics

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -2,14 +2,20 @@ import React from 'react';
 class SearchBar extends React.Component
 {
     state={term: ''};
-    
+
+    onFormSubmit= event =>{
+        event.preventDefault();
+        this.props.onSubmit(this.state.term); //we are taking elements here
+    };
     render(){
         return(
         <div className="ui segment">
-            <form className="ui form">
+            <form onSubmit={this.onFormSubmit} className="ui form">
                 <div className="field">
                     <label>Search form!</label>
-                    <input type="text" value={this.state.term} onChange={(e)=>this.setState({term: e.target.value})}/>
+                    <input type="text"
+                     value={this.state.term}
+                      onChange={(e)=>this.setState({term: e.target.value})}/>
             </div>
             </form>
         </div>);
--- pics/src/components/app.js
+++ pics/src/components/app.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
-const App=()=>{
-return (<div>
-   <SearchBar/>
-</div>);
-};
+
+class App extends React.Component
+{
+   onSearchSubmit(term){
+      axios.get('https://api.unsplash.com/search/photos',
+      {
+         params:{query: term}, //parameter query passed and given it value as term
+headers:{
+   Authorization: 'Client-ID DCi_gHO2f_G1ahBxWdMTYbey19TTaY3DN_7Z08zSkiQ'
+}
+      });
+   }
+   render()
+   {
+      return(
+         <div className="ui container">
+         <SearchBar onSubmit={this.onSearchSubmit}/>
+         </div>
+      )
+   }
+}
 
 export default App;
