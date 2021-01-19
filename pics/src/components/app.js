b33fd42031fa2a77c0ad6340b8d73be92ee7b2e5
agarwalminks <61506798+agarwalminks@users.noreply.github.com>
2021-01-19 16:06:04 +0530

    await and async

--- pics/src/components/app.js
+++ pics/src/components/app.js
@@ -4,14 +4,17 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component
 {
-   onSearchSubmit(term){
-      axios.get('https://api.unsplash.com/search/photos',
+  async onSearchSubmit(term){
+     const response= await axios
+      .get('https://api.unsplash.com/search/photos',
       {
          params:{query: term}, //parameter query passed and given it value as term
 headers:{
-   Authorization: 'Client-ID DCi_gHO2f_G1ahBxWdMTYbey19TTaY3DN_7Z08zSkiQ'
+   Authorization:
+    'Client-ID DCi_gHO2f_G1ahBxWdMTYbey19TTaY3DN_7Z08zSkiQ'
 }
-      });
+      });      
+      console.log(response.data.results); //called arrow function
    }
    render()
    {
