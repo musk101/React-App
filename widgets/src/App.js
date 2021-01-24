9a226a93e64a609fe4cb1beaaa8aa6f89fe7ca28
agarwalminks <61506798+agarwalminks@users.noreply.github.com>
2021-01-24 16:19:29 +0530

    semantic css

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items=[
+    {
+    title: 'what is react',
+    content: 'React is awesome!'
+    },
+    {
+        title: 'why use react?',
+        content: 'React is awesome!'
+        },
+        {
+            title: 'how do you use react',
+            content: 'Bruhh!'
+            },
+
+];
 
 export default ()=>{
-    return <div>
-        <h1>Hi!</h1></div>
+    return (
+    <div>
+        <Accordion items={items}/>
+        </div>
+        );
 };
 
